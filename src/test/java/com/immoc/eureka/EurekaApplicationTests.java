a2034740f6510f094c15c03d3c46d8b7388802e6
渠秀娟 <quxiujuan@xiniunet.com>
2019-10-30 23:43:08 +0800

    注册中心配置，取消自注册

--- src/test/java/com/immoc/eureka/EurekaApplicationTests.java
+++ src/test/java/com/immoc/eureka/EurekaApplicationTests.java
@@ -1,7 +1,8 @@
 package com.immoc.eureka;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
 class EurekaApplicationTests {
